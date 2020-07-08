df3fd75aaa675d226ffa9057b6389d8ac1cd5830
nina-sensei <1207035.h@gmail.com>
2020-07-08 10:27:38 +0900

    提出用

---
+++ dentist_top.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include("functions.php");
+check_session_id();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>歯科医師top</title>
+</head>
+
+<body>
+   <h1>歯科医師top</h1>
+   <a href="logout.php">Sign Out</a>
+</body>
+
+</html>
---
+++ technician_top.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include("functions.php");
+check_session_id();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>技工士top</title>
+</head>
+
+<body>
+   <h1>技工士top</h1>
+   <a href="logout.php">Sign Out</a>
+</body>
+
+</html>
